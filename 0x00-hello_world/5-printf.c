ff5bef762f4965f0c1e58be9cc674467bf36d7f5
Summayah001 <ganiyusummayah2019@gmail.com>
2022-09-10 07:06:17 -0700

    this is c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 
 /**
- *
- *Return: Always 0 (Success)
- */
+ * 
+ * main - the programme starts and ends with the main
+ *Return:the main returns 0 upon completion and success
+*/
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
